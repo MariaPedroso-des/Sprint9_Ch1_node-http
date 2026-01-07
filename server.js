916ebb5dfb14ac634531995978888f11f6410b6e
MariaPedroso-des <marhiapm@gmail.com>
2026-01-07 21:40:38 +0100

    2

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 //Siempre será node ./el inicio. Se puede llamar también index.js
 
 import http from 'node:http'
-
-
 import { obj } from './data.js'
+
+const PORT = 3000
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'content-Type': 'text/html; charset=utf-8'})
-  res.end(
-    `<!DOCTYPE html>
+  const template =`
+    <!DOCTYPE html>
     <html lang="es"
     <head>
       <meta charset="UTF-8">
       <title>${obj.title}</title>  
     </head>
     <body>
-      <h1>${obj.title}</h1>
+      <h1>${obj.title}tu</h1>
       <h2>${obj.subtitle}</h2>
       <p>${obj.description}</p>
     </body>
     </html>
-    `)
+  ` 
+  res.end(template)
 })
 
-server.listen(0, () => {
-  console.log(`server listen on port http://localhost:${server.address().port}`)
+server.listen(3000, () => {
+  console.log(`server listen on port http://localhost:${PORT}`)
 })
